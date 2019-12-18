125919d5f0a1b58c25414376b576514ac535cfd3
frunt <frunt@DESKTOP-02KKATA>
2019-12-18 15:59:46 +0200

    Добавьте файлы проекта.

---
+++ CompositePattern/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    class Client
+    {
+        public ICompositble Composite { get; set; }
+
+        public Client(ICompositble composite)
+        {
+            Composite = composite;
+        }
+
+        public int GetInfo()
+        {
+            return Composite.Operation();
+        }
+        
+        public void AddToComposite(ICompositble compositble)
+        {
+            if (Composite.IsComposite())
+            {
+                ((EntityComposite)Composite).Add(compositble);
+            }
+
+            //if (Composite.GetType() == typeof(EntityComposite))
+            //{
+            //    ((EntityComposite)Composite).Add(compositble);
+            //}
+        }
+    }
+}
